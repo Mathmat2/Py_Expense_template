c1b512a442bb15c4c6533bb8cc2c8bd41684c6c0
Mathieu Bouchet <mathieu.bouchet@epita.fr>
2021-11-03 20:55:59 +0100

    Add involved people in the expense

--- expense.py
+++ expense.py
@@ -1,6 +1,12 @@
 from PyInquirer import prompt
 import csv
 
+# Allow to delete chosen spender from involved people list
+def delete_spender_from_involved_choice(answers):
+    spender = answers['spender']
+    expense_questions[3]['choices'].remove({'name' : spender})
+    return True
+
 expense_questions = [
     {
         "type":"input",
@@ -18,7 +24,13 @@ expense_questions = [
         "message":"New Expense - Spender: ",
         "choices":[],
     },
-
+    {
+        "type":"checkbox",
+        "name":"involved",
+        "message":"New Expense - Involved People: ",
+        "choices":[],
+        "when":delete_spender_from_involved_choice
+    },
 ]
 
 
@@ -37,11 +49,21 @@ def new_expense(*args):
 
     expense_questions[2]['choices'] = users
 
+    involved_people = []
+    for user in users:
+        involved_people.append({'name': user})
+
+    print(involved_people)
+    
+    expense_questions[3]['choices'] = involved_people
+
     infos = prompt(expense_questions)
+
+    infos['involved'].append(infos['spender'])
     
     with open('expense_report.csv', 'a', newline='') as expenses_file:
         expenses_writer = csv.writer(expenses_file)
-        expenses_writer = expenses_writer.writerow([infos['label'], infos['amount'], infos['spender']])
+        expenses_writer = expenses_writer.writerow([infos['label'], infos['amount'], infos['spender'], infos['involved']])
 
     print("Expense Added !")
     return True
